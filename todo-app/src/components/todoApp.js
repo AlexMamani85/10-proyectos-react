36d3409cdae5c9f6edc84cedc766756ffd76869d
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-08-24 22:51:19 -0500

    CHORE: until print todos

--- todo-app/src/components/todoApp.js
+++ todo-app/src/components/todoApp.js
@@ -1,7 +1,92 @@
+import styled from '@emotion/styled'
+import React, { useState } from 'react';
+
+
+const Container = styled.div`
+  padding: 32px;
+  background-color: red;
+  text-align: center;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  width: 50%;
+  &:hover {
+    color: white;
+  }
+`
+
+const TodosContainer = styled.div`
+  padding: 32px;
+  background-color: green;
+  text-align: center;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  width: 50%;
+  &:hover {
+    color: white;
+  }
+`
+
+const Form = styled.form`
+
+`
+
+const Input = styled.input`
+type: ${({ type }) => type};
+value: ${({value}) => value};
+`
 
 function TodoApp() {
-    return ( <h1>Hola</h1>);
+
+  const [title, setTitle] = useState("Hola222");
+  const [todos, setTodos] = useState([]);
+  
+
+  function handleChange(e){
+    const value = e.target.value;
+    setTitle(value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: title,
+      completed: false.valueOf,
+    }
+    setTodos([...todos, newTodo]);
+
+
+  }
+
+
+    return ( <Container>
+      <Form>
+        <Input onChange={handleChange} 
+            value={title} />
+        <Input 
+          onClick={handleSubmit} 
+          type="submit" 
+          value="Create toDO"/>
+        {title}
+      </Form>
+
+      <TodosContainer>
+        {todos.map(item => (
+            <div key={item.id}>
+              {item.title}
+            </div>
+
+        ))}
+      </TodosContainer>
+
+
+
+    </Container>);
 }
 
-export default ;
+export default TodoApp;
 
