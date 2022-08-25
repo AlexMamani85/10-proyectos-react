e0a88d8c9979a8cf704741df5441ef2a8ad5138a
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-08-24 22:47:02 -0500

    CHORE: TodoApp in App

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoApp from './components/todoApp';
 
 function App() {
   return (
-    
+    <>
+      <TodoApp />
+    </>
+
   );
 }
 
