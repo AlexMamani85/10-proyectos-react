e5380623ef1882e1992e05f78f3ef791dc3911a8
AlexMamani85 <alex.mamani@tecsup.edu.pe>
2022-08-25 13:36:41 -0500

    CHORE: FormEdit and updateFunction

--- todo-app/src/components/todo.js
+++ todo-app/src/components/todo.js
@@ -4,6 +4,33 @@ export default function Todo({item}) {
     const [isEdit,setIsEdit] = useState(false)
 
 
+    function updateFunction(e){
+        e.preventDefault();
+        setIsEdit(false);
+        console.log(isEdit);
+    }
+    function FormEdit({value}) {
+
+        const [newValue, setNewValue] = useState(value)
+
+        function handleSubmit(e) {
+            e.preventDefault();
+
+        }
+        
+        function handleChange(e) {
+            const value = e.target.value;
+            setNewValue(value);
+        }
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <input  type='text' onChange={handleChange} value={newValue}/>
+                <button onClick={updateFunction}>Update</button>
+            </form>
+        );
+    }
+
     function TodoElement() {
         return (
             <div>
